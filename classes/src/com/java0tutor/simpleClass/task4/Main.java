dc46f8447fba7f07c4868a3487faf33304254d1b
hverda <nasoviva@gmail.com>
2022-05-26 14:50:42 -0500

    new tasks is ready

--- classes/src/com/java0tutor/simpleClass/task4/Main.java
+++ classes/src/com/java0tutor/simpleClass/task4/Main.java
@@ -94,9 +94,9 @@ public class Main {
 
 	public static int compareDestination(String s1, String s2) {
 		for (int i = 0; i < s1.length(); i++) {
-			if (s1.charAt(i) - s2.charAt(i) > 0)
+			if (i < s2.length() && s1.charAt(i) - s2.charAt(i) > 0)
 				return 1;
-			else if (s1.charAt(i) - s2.charAt(i) < 0)
+			else if (i < s2.length() || s1.charAt(i) - s2.charAt(i) < 0)
 				return -1;
 		}
 		return 0;
